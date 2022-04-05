d7932e57d5ceded4f34b8f81abd1c3a23580b831
lsadlerx <lsadlerx@LSADLERX-MOBL>
2022-04-05 16:03:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+void GetDirectoryLocation(string path)
+{
+
+}
+
+//================ MAIN()
+// See https://aka.ms/new-console-template for more information <---this is weird but okay
+Console.WriteLine("Hello, World!");
+
+
+
